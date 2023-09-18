a92e042d86c705fe06caba9eb9e52f8c955e84d1
Mariusz Pietrzak <C:\Users\prote\AppData\Roaming\The Bat! Pwd>
2023-09-18 11:06:08 +0200

    Adding main files
    All the files related to this project

---
+++ Odcinek7-myCanvas1.js
@@ -0,0 +1,369 @@
+let DOMContentLoaded = false;
+document.addEventListener("DOMContentLoaded", function() {
+    if (!DOMContentLoaded) {
+        DOMContentLoaded = true;
+
+        //contents - CTRL + F
+        // 1. Function to draw the arrow
+
+        // 2. The condition is responsible for draw and counting turn of the game
+
+        // 3. The condition is responsible for drawing 5 rectangles with white lines
+
+        // 4. Function getAlgorithmColor
+
+        // 5. Window addEventListener from Odcinek7-myCanvas2.js connecting click-event rectangles from
+        // that file with the
+        // rectangles from this file
+
+        //6. Paint rectangle event start
+        //6.1 The previous array will be wiped-out
+        //6.2 - Clear all the rectangles generated from previous round
+        //6.3 Alghoritm color which is the main engine the entire game
+        //6.4 Alghoritm color adding ok face under rectangles
+        //6.5 Alghoritm color adding blue face under rectangles
+        //6.6 Alghoritm color adding red face under rectangles - 276/300
+
+        // 7. Draw rectangles with suitable color from the previous turn, reset alghoritmColor2, detaling
+
+        //8. Delete previous color of rectangle after click the rectangle which has name "popraw",
+        // in addition to delete the previous record in alghoritmColor2 array.
+
+        // 9. Function clear arrow
+
+
+        let canvas = document.getElementById("myCanvas1");
+        let context = canvas.getContext("2d");
+
+        let rectWidth = 100;
+        let rectHeight = 100;
+        let startX = 60;
+        let startY = 10;
+
+        canvas.style.width = "123%";
+
+
+
+// 1. Function to draw the arrow
+        function drawArrow(initialStartX, initialStartY, rectWidth, rectHeight) {
+            context.beginPath();
+            context.strokeStyle = "white";
+            initialStartX = initialStartX + 150;
+
+            // Draw the arrow shape
+            context.moveTo(initialStartX + rectWidth / 2, initialStartY + rectHeight + 12);
+            context.lineTo(initialStartX + rectWidth / 2 + 8, initialStartY + rectHeight + 25);
+            context.lineTo(initialStartX + rectWidth / 2 - 8, initialStartY + rectHeight + 25);
+            context.closePath();
+
+            context.lineWidth = 6;
+            context.moveTo(initialStartX + rectWidth / 2, initialStartY + rectHeight + 15);
+            context.lineTo(initialStartX + rectWidth / 2, initialStartY + rectHeight + 40);
+            context.stroke();
+        }
+
+
+        drawArrow(startX - 150, startY, rectWidth, rectHeight);
+
+        var text = 1;
+
+        for (let i = 0; i <= 5; i++) {
+
+            if (i === 5) {
+                //2. The condition is responsible for draw and counting turn of the game
+                startX = startX + 60;
+                startY = 43;
+                const radius = 40;
+                const fillColor = "black";
+                const borderColor = "gold"; // Border color
+                const borderWidth = 2;      // Border width
+                context.arc(startX, startY, radius, 0, 2 * Math.PI, false);
+                context.fillStyle = fillColor;
+                context.fill();
+
+                context.strokeStyle = borderColor;
+                context.lineWidth = borderWidth;
+                context.stroke();
+
+                context.fillStyle = "gold";
+
+                context.font = "40px Arial";
+                context.textAlign = "center";
+                context.textBaseline = "middle";
+                window.addEventListener("message", function (event) {
+                    if (event.data.action === "brameczka5"){
+                        text++;
+                        context.clearRect(850, 24, 50, 36)
+                        context.fillStyle = "gold";
+                        context.font = "40px Arial";
+                        context.fillText(text, startX, startY);
+                    }
+                })
+                context.fillText(text, startX, startY);
+
+
+                context.fillStyle = "gold";
+                const text2 = "RUNDA";
+                context.font = "20px Arial";
+                context.textAlign = "center";
+                context.textBaseline = "middle";
+                context.fillText(text2, startX, startY + radius + 25);
+
+
+            } else {
+//3. The condition is responsible for drawing 5 rectangles with white lines
+                context.beginPath();
+                const initialStartY = startY;
+                context.fillStyle = "#2A292B";
+                context.fillRect(startX, startY, rectWidth, rectHeight);
+
+                context.fillStyle = "black";
+                context.fillRect(startX, startY, rectWidth, rectHeight);
+
+                context.strokeStyle = "white";
+                context.lineWidth = 2;
+                context.strokeRect(startX, startY, rectWidth, rectHeight);
+                context.closePath();
+
+
+                startX = startX + 150;
+
+            }
+        }
+        var color = ["red", "green", "blue", "saddlebrown", "yellow", "fuchsia", "grey"];
+
+
+
+        let brameczka = 0;
+
+        let algorithmColor = [];
+
+        //4. Function getAlgorithmColor
+        function getAlgorithmColor(algoritm, max) {
+            for (let i = 0; i < 5; i++) {
+                let randomNumber;
+                randomNumber = Math.floor(Math.random() * max);
+                algoritm.push(color[randomNumber]);
+            }
+        }
+
+
+        getAlgorithmColor(algorithmColor, 7);
+        console.log(algorithmColor);
+        const algorithmColorSafe = JSON.stringify(algorithmColor);
+        localStorage.setItem('algorithmColor', algorithmColorSafe);
+
+        let getAlgorithmColors = false;
+        let alghoritmColor2 = [];
+        let initialStartY = 10;
+        let okFace = 0;
+        let blueFace = 0;
+        let redFace = 0;
+
+        // 5. Window addEventListener from Odcinek7-myCanvas2.js connecting click-event rectangles from that file with the
+        //rectangles from this file
+        window.addEventListener("message", function (event) {
+            if (event.data.action === "paintRectangle") {
+                //6. Paint rectangle event start
+                brameczka++;
+
+                //6.1 The previous array will be wiped-out
+                clearArrow();
+
+
+                if (brameczka <= 5 && brameczka >= 1) {
+                    //6.2 - Clear all the rectangles generated from previous round
+                    context.clearRect(0, 190, 132, 175);
+
+                    if (brameczka === 1){
+                        alghoritmColor2 = [];
+                    }
+
+                    let initialStartX = event.data.x;
+                    context.fillStyle = event.data.color;
+
+                    alghoritmColor2.push(event.data.color);
+
+                    //6.3 Alghoritm color which is the main engine the entire game
+                    if (brameczka === 5){
+
+                        //Restoring algorithmColor - Give possibility playing in next rounds
+                        if (getAlgorithmColors === true){
+                            var string = localStorage.getItem('algorithmColor');
+                            algorithmColor = JSON.parse(string);
+                            getAlgorithmColors = false;
+                        }
+
+                        //Game win
+                        if (algorithmColor.every((color, index) => color === alghoritmColor2[index])) {
+                            window.parent.postMessage({
+                                action: "gameWin"
+                            }, "*")
+                    }
+                        //Faces getting closer to victory
+                        else{
+                            const OkFaceSafe = JSON.stringify(alghoritmColor2);
+                            localStorage.setItem('alghoritmColor2', OkFaceSafe);
+
+
+
+                            let image = new Image();
+                                image.src = "zdjęcia/ok.png";
+
+                            let image2 = new Image();
+                            image2.src = "zdjęcia/elsewhere.png";
+
+                            let image3 = new Image();
+                            image3.src = "zdjęcia/none.png";
+
+                                let initialStartX2 = 80;
+                                let initialStartX3 = 80;
+                                let initialStartX4 = 80;
+
+                            // 6.4 Alghoritm color adding ok face under rectangles 201/216
+                            for (let i = algorithmColor.length - 1; i >= 0; i--) {
+
+                                if (algorithmColor[i] === alghoritmColor2[i]) {
+
+                                    okFace++;
+
+                                    image.onload = function () {
+                                        for (let j = 0; j < okFace; j++) {
+                                            context.drawImage(image, initialStartX2, initialStartY + 290, 60, 60);
+                                            initialStartX2 = initialStartX2 + 150;
+                                        }
+
+                                    }
+                                            algorithmColor.splice(i, 1);
+                                            alghoritmColor2.splice(i, 1);
+                                }
+                            }
+
+                            //6.5 Alghoritm color adding blue face under rectangles - 218/262
+                            for (let i = 0; i < 5; i++) {
+                                if (algorithmColor.includes(alghoritmColor2[i])) {
+                                        blueFace++;
+
+                                    image2.onload = function () {
+                                        for (let j = 0; j < blueFace; j++) {
+                                            if(j === 0) {
+                                                initialStartX3 = 80 + 150 * okFace;
+                                            context.drawImage(image2, initialStartX3, initialStartY + 290, 60, 60);
+                                                }
+                                            else {
+                                                context.drawImage(image2, initialStartX3, initialStartY + 290, 60, 60);
+                                            }
+
+
+                                            initialStartX3 = initialStartX3 + 150;
+                                        }
+
+                                    }
+
+                                        algorithmColor.splice(algorithmColor.indexOf(alghoritmColor2[i]),1);
+                                }
+
+                            }
+                            var string2 = localStorage.getItem('alghoritmColor2');
+                            alghoritmColor2 = JSON.parse(string2);
+
+                            //6.6 Alghoritm color adding red face under rectangles - 276/300
+                            for (let i = 0; i < 5; i++) {
+
+                                    if (okFace + blueFace + redFace === 5){
+                                        break;
+                                    }
+                                    redFace++;
+
+                                    image3.onload = function () {
+                                        for (let j = 0; j < redFace; j++) {
+                                            if(j === 0) {
+                                                initialStartX4 = 80 + 150 * blueFace + 150 * okFace;
+                                                context.drawImage(image3, initialStartX4, initialStartY + 290, 60, 60);
+                                            }
+                                            else {
+                                                context.drawImage(image3, initialStartX4, initialStartY + 290, 60, 60);
+                                            }
+
+
+                                            initialStartX4 = initialStartX4 + 150;
+                                        }
+                                }
+
+                            }
+                            getAlgorithmColors = true;
+
+
+
+                        }
+
+                    }
+
+
+                    context.clearRect(initialStartX - 5, initialStartY - 7, rectWidth + 30, rectHeight + 30);
+                    context.fillRect(initialStartX, initialStartY, rectWidth, rectHeight);
+
+                    if (brameczka !== 5) {
+                        okFace = 0;
+                        blueFace = 0;
+                        redFace = 0;
+                        drawArrow(initialStartX, initialStartY, rectWidth, rectHeight);
+                    }
+
+                    else if (brameczka === 5) {
+                        //7. Draw rectangles with suitable color from the previous turn, reset alghoritmColor2
+                        //detaling
+                        brameczka = 0;
+
+                        for (let i = 4; i >= 0; i--) {
+
+                            context.fillStyle = "black";
+                            context.fillRect(initialStartX, initialStartY, rectWidth, rectHeight);
+                            context.strokeStyle = "white";
+                            context.lineWidth = 2;
+                            context.strokeRect(initialStartX, initialStartY, rectWidth, rectHeight);
+
+                            context.beginPath();
+                            context.fillStyle = alghoritmColor2[i];
+                            context.fillRect(initialStartX, initialStartY + 180, rectWidth, rectHeight);
+                            initialStartX = initialStartX - 150;
+                            alghoritmColor2.pop();
+
+                        }
+
+                        drawArrow(-90, initialStartY, rectWidth, rectHeight);
+                        window.parent.postMessage({
+                            action: "brameczka5"
+                        }, "*");
+                    }
+                }
+
+            } else if (event.data.action === "deleteRectangle") {
+                //8. Delete previous color of rectangle after click the rectangle which has name "popraw",
+                // in addition to delete the previous record in alghoritmColor2 array.
+                if (brameczka >= 1) {
+                    brameczka--;
+                    let initialStartX = event.data.x;
+                    context.fillStyle = "black";
+                    context.fillRect(initialStartX, initialStartY, rectWidth, rectHeight);
+                    alghoritmColor2.pop();
+                    context.strokeStyle = "white";
+                    context.lineWidth = 2;
+                    context.strokeRect(initialStartX, initialStartY, rectWidth, rectHeight);
+                    clearArrow();
+                    drawArrow(initialStartX - 150, initialStartY, rectWidth, rectHeight);
+                } else {
+                    brameczka = 0;
+                }
+
+            }
+
+        });
+
+        //9. Function clear arrow
+        function clearArrow() {
+            context.clearRect(90, 115, canvas.width, canvas.height);
+        }
+
+    }
+});
---
+++ Odcinek7-myCanvas2.js
@@ -0,0 +1,184 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var canvas2 = document.getElementById("myCanvas2");
+    var context2 = canvas2.getContext("2d");
+
+
+    //contents - CTRL + F
+    // 1. Responsive for web size
+
+    // 2. Loop to draw rectangles and store positions
+    // 2.1 - Creating the "popraw" rectangle resposible for restoring previous clicked rectangle
+
+    // 3. Resposible for giving cursor:pointer in correct position
+
+    // 4. Responsible for drawing rectangle in canvas1 with the same color which user has clicked in canvas2
+
+    var color = ["red", "green", "blue", "saddlebrown", "yellow", "fuchsia", "grey"];
+
+
+    //1. Responsive for web size
+    var rectWidth = 100;
+    var rectHeight = 100;
+
+
+    var canvasHeight = canvas2.height;
+
+        if (window.innerWidth >= 1400) {
+            rectWidth = 100;
+            rectHeight = 100;
+            var startX = 0;
+        } else if (window.innerWidth < 1400 && window.innerHeight + 139 > 600) {
+            let canvasWidth = canvas2.width;
+            rectWidth = 70;
+            rectHeight = 70;
+            startX = (canvasWidth - rectWidth * 8 - 140) / 2 - 90;
+        }
+
+        else if (window.innerHeight + 139 === 600){
+            let canvasWidth = canvas2.width;
+            rectWidth = 50;
+            rectHeight = 50;
+            startX = (canvasWidth - rectWidth * 8 - 140) / 2 - 70;
+        }
+
+    var startY = (canvasHeight - rectHeight) / 4;
+
+// Store rectangle positions for hit testing
+    var rectangles = [];
+
+
+    window.addEventListener("message", function (event) {
+        if (event.data.action === "gameWin") {
+            console.log("Game Win!");
+            document.getElementById("tekst2").innerHTML = "Wygrałeś! <span id='Jeszcze-raz' onclick='location.reload()'>Jeszcze raz?</span>"
+            context2.clearRect(0, 0, 1110, 120);
+            let disabledElements = document.querySelectorAll(".disabled");
+
+// Iteruj przez elementy i ustaw regułę "pointer-events" na "none"
+            for (let i = 0; i < disabledElements.length; i++) {
+                disabledElements[i].style.pointerEvents = "none";
+            }
+        }
+    });
+
+    // onclick='location.reload()'
+
+    // 2. Loop to draw rectangles and store positions
+    for (let i = 0; i < color.length + 1; i++) {
+
+        context2.fillStyle = color[i];
+        if (window.innerWidth >= 1400) {
+            startX = startX + 120;
+        }else if (window.innerWidth < 1400 && window.innerHeight + 139 > 600) {
+            startX = startX + 90;
+        }
+        else if (window.innerHeight <= 1280 && window.innerHeight + 139 === 600) {
+            startX = startX + 70;
+        }
+
+        context2.fillRect(startX, startY, rectWidth, rectHeight);
+        rectangles.push({x: startX, y: startY, width: rectWidth, height: rectHeight});
+
+        //2.1 - Creating the "popraw" rectangle resposible for restoring previous clicked rectangle
+        if (i === color.length) {
+            context2.fillStyle = "#2A292B";
+            context2.fillRect(startX, startY, rectWidth, rectHeight);
+
+            // Adding a border to the last rectangle
+            context2.strokeStyle = "gray"; // Set the border color
+            context2.lineWidth = 1; // Set the border width
+            context2.strokeRect(startX, startY, rectWidth, rectHeight);
+
+            // Adding text inside the last rectangle
+            context2.fillStyle = "gray"; // Set the text color
+            const text = "popraw"; // The text to display
+            const textX = startX + rectWidth / 2; // X coordinate for text centering
+            const textY = startY + rectHeight / 2; // Y coordinate for text centering
+            if (window.innerHeight <= 1280 && window.innerHeight + 139 === 600){
+                context2.font = "12px Arial"; // Set the font size and type
+            }
+            else {
+                context2.font = "17px Arial"; // Set the font size and type
+            }
+
+            context2.textAlign = "center"; // Center the text horizontally
+            context2.textBaseline = "middle"; // Center the text vertically
+            context2.fillText(text, textX, textY);
+        }
+    }
+
+
+//3. Resposible for giving cursor:pointer in correct position
+    canvas2.addEventListener("mousemove", function (event){
+
+
+
+        var rect = canvas2.getBoundingClientRect();
+        var mouseX = event.clientX - rect.left;
+        var mouseY = event.clientY - rect.top;
+
+        var cursorStyle = "default";
+
+        for (let i = 0; i < rectangles.length; i++) {
+            if (
+            mouseX >= rectangles[i].x && mouseX <= rectangles[i].x + rectangles[i].width &&
+            mouseY >= rectangles[i].y && mouseY <= rectangles[i].y + rectangles[i].height
+        ){
+                cursorStyle = "pointer";
+
+            }
+
+        }
+
+
+        canvas2.style.cursor = cursorStyle;
+
+    })
+
+    //4. Responsible for drawing rectangle in canvas1 with the same color which user has clicked in canvas2
+    let positionX = 60;
+    canvas2.addEventListener("click", function(event) {
+        const clickX = event.clientX - canvas2.getBoundingClientRect().left;
+        const clickY = event.clientY - canvas2.getBoundingClientRect().top;
+        for (let i = 0; i < rectangles.length; i++) {
+            const rect = rectangles[i];
+            if (clickX >= rect.x && clickX <= rect.x + rectWidth &&
+                clickY >= rect.y && clickY <= rect.y + rectHeight) {
+                const selectedColor = color[i]; // Assuming colors is an array of color values
+                context2.fillStyle = selectedColor;
+                // Send message with color and position
+                if (i < 7 && positionX < 60 + 150 * 5) {
+                    positionX = positionX + 150;
+                    window.addEventListener("message", function (event) {
+                        if (event.data.action === "brameczka5"){
+                            positionX = 60;
+                        }
+                    })
+                    window.parent.postMessage({
+                        action: "paintRectangle",
+                        x: positionX - 150,
+                        color: selectedColor
+                    }, "*");
+                }
+                else {
+                    if (positionX > 60){
+                        positionX = positionX - 150;
+                        window.parent.postMessage({
+                            action: "deleteRectangle",
+                            x: positionX,
+                            color: selectedColor
+                        }, "*");
+                    }
+
+                }
+break;
+            }
+        }
+    });
+
+
+
+
+});
+
