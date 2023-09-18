8fa8090dcc509bdf04cd29cb3673e60333d47460
Mariusz Pietrzak <C:\Users\prote\AppData\Roaming\The Bat! Pwd>
2023-09-18 15:15:24 +0200

    cleaning code
    Cleaning code

--- Odcinek7-myCanvas1.js
+++ Odcinek7-myCanvas1.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (!DOMContentLoaded) {
         DOMContentLoaded = true;
 
+        //This is my modificaction
         //contents - CTRL + F
         // 1. Function to draw the arrow
 
